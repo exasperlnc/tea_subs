385ca1fd00b1c44e407bb353d0d81fcaa2fa4a14
Logan Cole <emtlogancole@gmail.com>
2023-08-02 15:24:31 -0500

    add tea and subscription seed data

--- README.md
+++ README.md
@@ -9,3 +9,4 @@
 - Realized after creating postman endpoints that the endpoint to 'delete' a subscription should be changed to a patch request
 - merged PR too early, PR's are easier to manage when they involve other people and an actual review process.
 - made dummy data for testing before it was needed 
+- remembered validates_presence_of doesn't like the "false" boolean
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Customer.create!(first_name: "Finn", last_name: "Smith", email: "kitt.e.mail@email.com", address: "456 Main St" )
-Customer.create!(first_name: "Ilsa", last_name: "VonLicktenstein", email: "ruff@email.com", address: "456 Center St" )
+c1 = Customer.create!(first_name: "Finn", last_name: "Smith", email: "kitt.e.mail@email.com", address: "456 Main St" )
+c2 = Customer.create!(first_name: "Ilsa", last_name: "VonLicktenstein", email: "ruff@email.com", address: "456 Center St" )
 
+t1 = Tea.create!(title: "Tea 1", description: "The First Tea", temperature: 1, brew_time: 1 )
+t2 = Tea.create!(title: "Tea 2", description: "The Second Tea", temperature: 2, brew_time: 2 )
+t3 = Tea.create!(title: "Tea 3", description: "The Third Tea", temperature: 3, brew_time: 3)
+t4 = Tea.create!(title: "Tea 4", description: "The Fourth Tea", temperature: 4, brew_time: 4)
+t5 = Tea.create!(title: "Tea 5", description: "The Fifth Tea", temperature: 5, brew_time: 5)
+t6 = Tea.create!(title: "Tea 6", description: "The Sixth Tea", temperature: 6, brew_time: 6)
+t7 = Tea.create!(title: "Tea 7", description: "The Seventh Tea", temperature: 7, brew_time: 7)
+
+Subscription.create!(customer: c1, tea: t1, title: t1.title, price: 1, status: false, frequency: 1)
+Subscription.create!(customer: c1, tea: t2, title: t2.title, price: 2, status: false, frequency: 2)
+Subscription.create!(customer: c1, tea: t3, title: t3.title, price: 3, status: true, frequency: 3)
+Subscription.create!(customer: c1, tea: t1, title: t1.title, price: 1, status: false, frequency: 1)
+Subscription.create!(customer: c1, tea: t4, title: t1.title, price: 4, status: true, frequency: 4)
