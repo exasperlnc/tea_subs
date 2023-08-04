699fc94e5a0ad1870360ca926c76cecd89f2a4ae
Logan Cole <emtlogancole@gmail.com>
2023-08-04 12:17:32 -0500

    add subscriptionfacade#cancel_subscription

--- app/facades/subscription_facade.rb
+++ app/facades/subscription_facade.rb
@@ -4,4 +4,8 @@ class SubscriptionFacade
     @tea = Tea.find(tea_id)
     @sub = Subscription.create(customer_id: customer_id, tea_id: tea_id, frequency: frequency, price: (frequency * 1), status: true, title: @tea.title)
   end
+
+  def cancel_subscription(sub)
+    sub.status = false
+  end
 end
