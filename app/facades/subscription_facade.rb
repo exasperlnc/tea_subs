84f2dd7903e10934b974fca8bf11d9d16198c5c9
Logan Cole <emtlogancole@gmail.com>
2023-08-04 12:07:39 -0500

    add subscription facade

--- app/facades/subscription_facade.rb
+++ app/facades/subscription_facade.rb
@@ -2,6 +2,6 @@ class SubscriptionFacade
   def create_subscription(customer_id, tea_id, frequency)
     # @customer = Customer.find(customer_id)
     @tea = Tea.find(tea_id)
-    @sub = Subscription.create(customer_id: customer_id, tea_id: tea_id, frequency: frequency, price: 5, status: true, title: @tea.title)
+    @sub = Subscription.create(customer_id: customer_id, tea_id: tea_id, frequency: frequency, price: (frequency * 1), status: true, title: @tea.title)
   end
 end
